6054abc28a7951a044c04493e5cf91c49ca01360
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2023-07-19 09:09:14 -0500

    Switch to SvelteKitSite SST Construct

--- stacks/Frontend.ts
+++ stacks/Frontend.ts
@@ -1,11 +1,9 @@
-import { Api, StackContext, Table, StaticSite } from "sst/constructs";
+import { StackContext, SvelteKitSite } from "sst/constructs";
 
 export function Frontend({ stack }: StackContext) {
   // Deploy the Svelte frontend
-  const site = new StaticSite(stack, "SvelteSkeleton", {
+  const site = new SvelteKitSite(stack, "SvelteSkeleton", {
     path: "packages/frontend",
-    buildCommand: "npm run build",
-    buildOutput: ".svelte-kit/output",
   });
 
   // Show the URLs in the output
