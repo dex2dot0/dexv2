0f7bf0ab6a43a2b27f1e88443af7b6e283ae9151
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2023-09-08 21:14:54 -0500

    DNS Updates
    Should hopefully resolve the issues.

--- stacks/DNS.ts
+++ stacks/DNS.ts
@@ -2,14 +2,14 @@ import { StackContext } from "sst/constructs";
 
 const baseDomain = process.env.BASE_DOMAIN;
 
-const getDomain = (appStage: string) => {
-    return appStage != "dev" ? `${appStage}.dev.${baseDomain}` : `dev.${baseDomain}`;
-};
+// const getDomain = (appStage: string) => {
+//     return appStage != "dev" ? `${appStage}.dev.${baseDomain}` : `dev.${baseDomain}`;
+// };
 
 export function DNS({ stack, app }: StackContext) {
-    const subDomain = getDomain(app.stage);
-    const zone = app.stage == "production" ? baseDomain : `dev.${baseDomain}`;
-    const domain = app.stage == "production" ? baseDomain : subDomain;
+    //const subDomain = getDomain(app.stage);
+    const zone = app.stage == "production" ? baseDomain : `${app.stage}.${baseDomain}`;
+    const domain = baseDomain;
 
     return {
         zone,
--- stacks/Frontend.ts
+++ stacks/Frontend.ts
@@ -1,20 +1,20 @@
 import { StackContext, SvelteKitSite, use } from "sst/constructs";
 import { DNS } from "./DNS";
 
-export function Frontend({ stack }: StackContext) {
+export function Frontend({ app, stack }: StackContext) {
   const dns = use(DNS);
 
   // Deploy the Svelte frontend
   const site = new SvelteKitSite(stack, "SvelteSkeleton", {
     customDomain: {
-      domainName: `*.${dns.domain}`,
-      hostedZone: dns.zone,
+      domainName: dns.zone,
+      hostedZone: dns.domain
     },
     path: "packages/frontend",
   });
 
   // Show the URLs in the output
   stack.addOutputs({
-    SiteUrl: site.url,
+    SiteUrl: site.customDomainUrl,
   });
 }
