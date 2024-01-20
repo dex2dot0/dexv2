2276f65cb873d9a2fcecf4586b316dd56a6ae36a
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2024-01-19 23:27:09 -0600

    Background Logos POC WIP

--- packages/frontend/src/routes/test/+page.svelte
+++ packages/frontend/src/routes/test/+page.svelte
@@ -1,20 +1,45 @@
 <script lang="ts">
-	export let numLogos: number = 100; // Number of logos
+	import { onMount } from 'svelte';
 	import Amplify from '../../components/brand-logos/Amplify.svelte';
-	let logos = Array.from({ length: numLogos }, (_, i) => i);
-	const initialRadiusPx = 220; // Initial radius in pixels
-	const baseRadiusIncrement = 5; // Base incremental increase in radius per logo
-	const angleIncrement = 16; // Angle increment in degrees
-	const baseSizeRem = 3; // Base size in rem for the first logo
-	const sizeIncrementPerLogo = 0.1; // Size increment per logo in rem
 
-	function getSize(index: number) {
-		const sizeIncrement = index > 20 ? 0.15 : sizeIncrementPerLogo;
-		return baseSizeRem + sizeIncrement * index;
+	export let numLogos: number = 100;
+	let logos = Array.from({ length: numLogos }, (_, i) => i);
+
+	let initialRadiusPx: number;
+	let baseRadiusIncrement = 5;
+	let angleIncrement = 16;
+	let baseSizeRem: number;
+	let sizeIncrementPerLogo: number;
+
+	onMount(() => {
+		// For mobile viewports will automatically cycle through the image sections
+		// rather than wait for hover to change the image
+		window.addEventListener('resize', handleResize);
+		handleResize();
+	});
+
+	function handleResize() {
+		const width = window.innerWidth;
+
+		if (width < 600) {
+			initialRadiusPx = 120;
+			baseSizeRem = 1.5;
+			sizeIncrementPerLogo = 0.15;
+		} else {
+			initialRadiusPx = 220;
+			baseSizeRem = 3;
+			sizeIncrementPerLogo = 0.1;
+		}
 	}
 
+	// Calculate size of each logo
+	function getSize(index: number) {
+		return baseSizeRem + sizeIncrementPerLogo * index;
+	}
+
+	// Calculate radius increment
 	function getRadiusIncrement(index: number) {
-		return baseRadiusIncrement + getSize(index) / 2; // Adjust this formula as needed
+		return baseRadiusIncrement + getSize(index) / 2;
 	}
 
 	// Calculate position for each logo in a spiral pattern
@@ -32,17 +57,19 @@
 
 <div class="absolute overflow-hidden" style="width: 100vw; height: 100vh;">
 	<!-- Logos positioned in a spiral pattern -->
-	{#each logos as _, index}
-		<div
-			class="absolute"
-			style={`left: 50%; top: 50%; transform: translate(${calculateSpiralPosition(index).x}px, ${
-				calculateSpiralPosition(index).y
-			}px)`}
-		>
-			<Amplify
-				style={`width: ${getSize(index)}rem; height: ${getSize(index)}rem;`}
-				className={`fill-tertiary-500 stroke-tertiary-800`}
-			/>
-		</div>
-	{/each}
+	{#if baseSizeRem && sizeIncrementPerLogo}
+		{#each logos as _, index}
+			<div
+				class="absolute"
+				style={`left: 50%; top: 50%; transform: translate(${calculateSpiralPosition(index).x}px, ${
+					calculateSpiralPosition(index).y
+				}px)`}
+			>
+				<Amplify
+					style={`width: ${getSize(index)}rem; height: ${getSize(index)}rem;`}
+					className={`fill-tertiary-500 stroke-tertiary-800`}
+				/>
+			</div>
+		{/each}
+	{/if}
 </div>
