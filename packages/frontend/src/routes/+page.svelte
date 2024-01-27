abba0481c6d6f29312aff4c74bfb86e64af974fa
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2024-01-26 23:37:35 -0600

    Delay Render
    TODO: Add Spinner

--- packages/frontend/src/routes/+page.svelte
+++ packages/frontend/src/routes/+page.svelte
@@ -94,73 +94,81 @@
 	};
 </script>
 
-<div class="absolute overflow-hidden -z-10" style="width: 100vw; height: 100vh;">
-	{#if mediaSize}
+{#if mediaSize}
+	<div class="absolute flex overflow-hidden -z-10 h-full w-full">
 		<BrandLogos {currentCategory} bind:mediaSize />
-	{/if}
-</div>
-<div class="h-full w-full flex items-center justify-center">
-	<div class="grid grid-cols-3 h-full w-full text-center align-middle">
-		<div>
-			<div id="11" on:mouseover={() => onHover(11)} class="h-1/3 flex items-center justify-center">
-				<span class={currentDivFocus === '11' ? 'focus-heading' : 'non-focus-heading'}>Cloud</span>
-			</div>
-			<div id="9" on:mouseover={() => onHover(9)} class="h-1/3 flex items-center justify-center">
-				<span class={currentDivFocus === '9' ? 'focus-heading' : 'non-focus-heading'}>DevOps</span>
-			</div>
-			<div
-				id="7"
-				on:mouseover={() => onHover(7)}
-				class="h-1/3 flex flex-col items-center justify-center"
-			>
-				<span class={currentDivFocus === '7' ? 'focus-heading' : 'non-focus-heading'}
-					>Serverless</span
+	</div>
+	<div class="h-full w-full flex items-center justify-center">
+		<div class="grid grid-cols-3 h-full w-full text-center align-middle">
+			<div>
+				<div
+					id="11"
+					on:mouseover={() => onHover(11)}
+					class="h-1/3 flex items-center justify-center"
 				>
-			</div>
-		</div>
-		<div>
-			<div
-				id="12"
-				on:mouseover={() => onHover(12)}
-				class="h-1/3 flex flex-col items-center justify-center"
-			>
-				<span class={currentDivFocus === '12' ? 'focus-heading' : 'non-focus-heading'}
-					>Full-Stack</span
+					<span class={currentDivFocus === '11' ? 'focus-heading' : 'non-focus-heading'}>Cloud</span
+					>
+				</div>
+				<div id="9" on:mouseover={() => onHover(9)} class="h-1/3 flex items-center justify-center">
+					<span class={currentDivFocus === '9' ? 'focus-heading' : 'non-focus-heading'}>DevOps</span
+					>
+				</div>
+				<div
+					id="7"
+					on:mouseover={() => onHover(7)}
+					class="h-1/3 flex flex-col items-center justify-center"
 				>
+					<span class={currentDivFocus === '7' ? 'focus-heading' : 'non-focus-heading'}
+						>Serverless</span
+					>
+				</div>
 			</div>
-			<div id="0" on:mouseover={() => onHover(0)} class="grid place-content-center w-full h-1/3">
-				<figure class="w-full h-full">
-					<section class="img-bg" />
-					<img width="200" height="200" src={imageSrc} alt="headshot" />
-				</figure>
-			</div>
-			<div
-				id="6"
-				on:mouseover={() => onHover(6)}
-				class="h-1/3 flex flex-col items-center justify-center"
-			>
-				<span class={currentDivFocus === '6' ? 'focus-heading' : 'non-focus-heading'}>Data</span>
-			</div>
-		</div>
-		<div>
-			<div id="1" on:mouseover={() => onHover(1)} class="h-1/3 flex items-center justify-center">
-				<span class={currentDivFocus === '1' ? 'focus-heading' : 'non-focus-heading'}>APIs</span>
-			</div>
-			<div
-				id="3"
-				on:mouseover={() => onHover(3)}
-				class="h-1/3 flex flex-col items-center justify-center"
-			>
-				<span class={currentDivFocus === '3' ? 'focus-heading bg-tertiary' : 'non-focus-heading'}
-					>WebDev</span
+			<div>
+				<div
+					id="12"
+					on:mouseover={() => onHover(12)}
+					class="h-1/3 flex flex-col items-center justify-center"
 				>
+					<span class={currentDivFocus === '12' ? 'focus-heading' : 'non-focus-heading'}
+						>Full-Stack</span
+					>
+				</div>
+				<div id="0" on:mouseover={() => onHover(0)} class="grid place-content-center w-full h-1/3">
+					<figure class="w-full h-full">
+						<section class="img-bg" />
+						<img width="200" height="200" src={imageSrc} alt="headshot" />
+					</figure>
+				</div>
+				<div
+					id="6"
+					on:mouseover={() => onHover(6)}
+					class="h-1/3 flex flex-col items-center justify-center"
+				>
+					<span class={currentDivFocus === '6' ? 'focus-heading' : 'non-focus-heading'}>Data</span>
+				</div>
 			</div>
-			<div id="4" on:mouseover={() => onHover(4)} class="h-1/3 flex items-center justify-center">
-				<span class={currentDivFocus === '4' ? 'focus-heading' : 'non-focus-heading'}>Agile</span>
+			<div>
+				<div id="1" on:mouseover={() => onHover(1)} class="h-1/3 flex items-center justify-center">
+					<span class={currentDivFocus === '1' ? 'focus-heading' : 'non-focus-heading'}>APIs</span>
+				</div>
+				<div
+					id="3"
+					on:mouseover={() => onHover(3)}
+					class="h-1/3 flex flex-col items-center justify-center"
+				>
+					<span class={currentDivFocus === '3' ? 'focus-heading bg-tertiary' : 'non-focus-heading'}
+						>WebDev</span
+					>
+				</div>
+				<div id="4" on:mouseover={() => onHover(4)} class="h-1/3 flex items-center justify-center">
+					<span class={currentDivFocus === '4' ? 'focus-heading' : 'non-focus-heading'}>Agile</span>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+{:else}
+	Loading...
+{/if}
 
 <style lang="postcss">
 	figure {
