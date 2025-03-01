01907d2dc15fedad4eb12fa000435a40cfd89358
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2025-02-28 23:46:54 -0600

    Add Blog
    Still need to setup deployment stack. for this

---
+++ packages/blog/src/consts.ts
@@ -0,0 +1,49 @@
+import type { Site, Page, Links, Socials } from '@types';
+
+// Global
+export const SITE: Site = {
+	TITLE: 'Dex Blog',
+	DESCRIPTION:
+		'Welcome to my blog! I write about software development, technology, and other topics I am passionate about.',
+	AUTHOR: 'Cliff Johnson',
+};
+
+// Blog Page
+export const BLOG: Page = {
+	TITLE: 'Blog',
+	DESCRIPTION: 'Writing on topics I am passionate about.',
+};
+
+// Search Page
+export const SEARCH: Page = {
+	TITLE: 'Search',
+	DESCRIPTION: 'Search all posts by keyword.',
+};
+
+// Links
+export const LINKS: Links = [
+	{
+		TEXT: 'Home Page',
+		HREF: 'https://dexv2.com',
+	},
+	{
+		TEXT: 'Blog',
+		HREF: '/',
+	},
+];
+
+// Socials
+export const SOCIALS: Socials = [
+	{
+		NAME: 'Github',
+		ICON: 'github',
+		TEXT: 'dex2dot0',
+		HREF: 'https://github.com/dex2dot0',
+	},
+	{
+		NAME: 'LinkedIn',
+		ICON: 'linkedin',
+		TEXT: 'Cliff Johnson',
+		HREF: 'https://www.linkedin.com/in/cliffjjohnson/',
+	},
+];
