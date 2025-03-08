a3773ab02f31b08b3d312ad270ab06eb89475134
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2025-03-07 21:39:55 -0600

    Switch Blog to StaticSite

--- stacks/Blog.ts
+++ stacks/Blog.ts
@@ -1,17 +1,19 @@
-import { AstroSite, StackContext, use } from 'sst/constructs';
+import { StaticSite, StackContext, use } from 'sst/constructs';
 import { DNS } from './DNS';
 
 export function DexBlog({ stack }: StackContext) {
 	const dns = use(DNS);
 
 	// Create the Astro site
-	const site = new AstroSite(stack, 'DexBlog', {
+	const site = new StaticSite(stack, 'DexBlog', {
 		customDomain: {
 			domainName: `blog.${dns.zone}`,
 			hostedZone: dns.domain,
+			domainAlias: `www.blog.${dns.zone}`,
 		},
 		path: 'packages/blog',
 		buildCommand: 'pnpm run build',
+		buildOutput: 'dist',
 	});
 
 	// Add the site's URL to stack output
