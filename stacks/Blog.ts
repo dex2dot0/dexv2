62ddb7579101df66e5c5476d1c8c8e406e842d87
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2025-03-07 20:31:10 -0600

    Add Blog Site

---
+++ stacks/Blog.ts
@@ -0,0 +1,20 @@
+import { AstroSite, StackContext, use } from 'sst/constructs';
+import { DNS } from './DNS';
+
+export default function DexBlog({ stack }: StackContext) {
+	const dns = use(DNS);
+
+	// Create the Astro site
+	const site = new AstroSite(stack, 'DexBlog', {
+		customDomain: {
+			domainName: `blog.${dns.zone}`,
+			hostedZone: dns.domain,
+		},
+		path: 'packages/blog',
+	});
+
+	// Add the site's URL to stack output
+	stack.addOutputs({
+		URL: site.url,
+	});
+}
